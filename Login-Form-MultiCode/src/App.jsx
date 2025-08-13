5057d7020e8f25a568e4159e77fb0d7ac9d5eaf5
Vishwakarma Abhishek <rashgulla200.1@gmail.com>
2025-08-13 14:58:56 +0530

    Design Element

--- Login-Form-MultiCode/src/App.jsx
+++ Login-Form-MultiCode/src/App.jsx
@@ -1,33 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
+import logo from './assets/logo.png' 
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="main-container">
+        <div className="left-part">
+          <img src={logo} alt="" className='logo'/>
+
+          <p>Say hello to global food and beverage producers and suppliers, all in one place</p>
+        </div>
+
+        <div className="right-part">
+          <p>Let's get started<br />with a few simple steps</p>
+
+          <form className='form-container'>
+            <label htmlFor="email">Email</label>
+            <input type="email" />
+
+            <label htmlFor="Full Name">FullName</label>
+            <input type="text" />
+
+            <label htmlFor="Password">Password</label>
+            <input type="password" />
+
+            <button>Sign up</button>
+          </form>
+
+          <div className="term-conditions">
+            <p className='term-text'>By signing up, you agree to our <a href="" style={{color:'rgb(151, 150, 150)'}}>Terms of Service.</a><br />Already have an account? <a href="#">Log in</a></p>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
